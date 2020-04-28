f25b2c35a077abd3148efba8988e1a7b1ef4352e
Gregory Brancq <gregory.brancq@free.fr>
2020-04-28 21:04:20 +0200

    Removed parsing line. Better logger information. Analyze results and print it in a dialog window.
    Change-Id: Ie212e6cfd5a511107b13d1d9e80b9ec36e1a03ae

--- convertBmp2Jpg.py
+++ convertBmp2Jpg.py
@@ -8,24 +8,28 @@ Convert bmp to jpg
 # Import
 import logging
 
-from log import createLog
+from common import createLog, parsingLine
 from nemoBase import NemoBase
 
 
 class ConvertBmp2Jpg(NemoBase):
-    def __init__(self, root_log):
+    def __init__(self, root_log, args):
         root_log_name = '.'.join([root_log, self.__class__.__name__])
         self.logCB = logging.getLogger(root_log_name)
         auth_ext = [".tif", ".TIF", ".gif", ".GIF", ".bmp", ".BMP"]
         res_ext = ".jpg"
-        super().__init__(root_log_name, auth_ext, res_ext)
+        msg_not_found = "No image has been found."
+        super().__init__(root_log, root_log_name, args, auth_ext, res_ext, msg_not_found)
 
 
 def main():
     # Create log class
     root_log = 'convertBmp2Jpg'
-    logger = createLog(root_log)
-    ConvertBmp2Jpg(root_log).run()
+    (parsedArgs, args) = parsingLine()
+    logger = createLog(root_log, parsedArgs)
+    logger.info("START")
+    ConvertBmp2Jpg(root_log, args).run()
+    logger.info("STOP")
 
 
 if __name__ == '__main__':
--- nemoBase.py
+++ nemoBase.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
@@ -8,59 +8,48 @@ import os
 import re
 import subprocess
 import sys
-from optparse import OptionParser
 
 import logging
 
-
-# sys.path.append('/home/greg/Greg/work/env/pythonCommon')
-# from message import MessageDialogEnd
-# from basic import getLogDir
+sys.path.append('/home/greg/Greg/work/env/pythonCommon')
+from basic import getLogDir
+from message import MessageDialogEnd
 
 
 class NemoBase:
-    def __init__(self, root_log, auth_ext, res_ext):
+    def __init__(self, log_name, root_log, arguments, auth_ext, res_ext, msg_not_found):
         self.logNB = logging.getLogger('.'.join([root_log, __name__]))
-        self.logNB.info("creating an instance")
+        self.prog_name = log_name
+        self.log_name = os.path.join(getLogDir(), log_name) + ".log"
+        self.arguments = arguments
         self.auth_ext = auth_ext
         self.res_ext = res_ext
-        self.warning = 0
+        self.msg_not_found = msg_not_found
         self.file_list = list()
-        self.parse_line = str()
-
-    def parsingLine(self):
-        parser = OptionParser()
-        parser.add_option(
-            "-d",
-            "--debug",
-            action="store_true",
-            dest="debug",
-            default=False,
-            help="Display all debug information"
-        )
-        (parsedArgs, self.arguments) = parser.parse_args()
+        self.msg_end = ""
+        self.error = False
 
     def addFile(self, file_name):
-        self.logNB.info("In  addFile file_name=" + str(file_name))
+        self.logNB.debug("In  addFile file_name=" + str(file_name))
         dir_n = os.path.dirname(file_name)
         dir_n1 = dir_n.replace('(', '\(')
         dir_n2 = dir_n1.replace(')', '\)')
         file_name_wo_dir = re.sub(dir_n2 + "\/", '', file_name)
         (fileN, extN) = os.path.splitext(file_name_wo_dir)
         if self.auth_ext.__contains__(extN):
-            self.logNB.info("In  addFile dir_n=" + str(dir_n) + ", fileN=" + str(fileN) + ", extN=" + str(extN))
+            self.logNB.debug("In  addFile dir_n=" + str(dir_n) + ", fileN=" + str(fileN) + ", extN=" + str(extN))
             self.file_list.append([dir_n, fileN, extN])
         else:
-            self.warning += 1
-            self.logNB.debug(
-                "In  addFile file " + str(file_name_wo_dir) + " is not a good extension as " + str(self.auth_ext))
+            self.msg_end += "File %s has not a good extension (%s)\n" % (file_name_wo_dir, str(self.auth_ext))
+            self.logNB.warning(
+                "In  addFile file %s has not a good extension (%s)" % (file_name_wo_dir, str(self.auth_ext)))
 
     def getFileList(self):
         self.logNB.debug("In  getFileList, arguments=%s" % str(self.arguments))
         if len(self.arguments) != 0:
             for file_or_dir in self.arguments:
                 if os.path.isdir(file_or_dir):
-                    self.logNB.debug("In  getFileList dir=" + str(file_or_dir))
+                    self.logNB.debug("In  getFileList, dir=%s" % str(file_or_dir))
                     for dir_path, dir_names, file_names in os.walk(file_or_dir):
                         for file_name in file_names:
                             self.addFile(os.path.join(dir_path, file_name))
@@ -69,46 +58,46 @@ class NemoBase:
                     self.logNB.debug("In  getFileList file=" + str(file_or_dir))
                     self.addFile(file_or_dir)
 
-        self.logNB.info("Out getFileList, file_list=" + str(self.file_list))
-
-    def checkFileList(self, msg):
-        if len(self.file_list) == 0:
-            self.logNB.error(msg)
-            sys.exit(1)
+        self.logNB.info("file_list=%s" % str(self.file_list))
 
     def convert(self):
-        self.logNB.info("In  convert")
         old_dir = os.getcwd()
 
         for (dir_name, file_name, file_ext) in self.file_list:
-            self.logNB.info("In  runJob directory " + str(dir_name) + "  runJob " + file_name + file_ext)
-
             if dir_name != "":
                 os.chdir(dir_name)
 
-            cmd = 'convert "' + file_name + file_ext + '" "' + file_name + self.res_ext + '"'
-            self.logNB.info("In  runJob cmd=" + str(cmd))
-            proc = subprocess.Popen(cmd, shell=True, stderr=subprocess.STDOUT)
-            proc.wait()
-            if proc.returncode != 0:
-                count_error += 1
-                self.logNB.error("In  runJob file: issue with " + str(os.path.join(dir_name, file_name + file_ext)))
+            cmd = ['convert', file_name + file_ext, file_name + self.res_ext]
+            self.logNB.info("Run command %s" % str(cmd))
+            process = subprocess.Popen(cmd, stderr=subprocess.STDOUT)
+            process.wait()
+            if process.returncode != 0:
+                self.error = True
+                self.msg_end += "In %s, cmd failed : %s\n" % (os.getcwd(), str(cmd))
+            else:
+                self.msg_end += "Converted : %s\n" % (os.path.join(os.getcwd(), file_name + self.res_ext))
 
             if dir_name != "":
                 os.chdir(old_dir)
 
-    def analyzeJob(self):
-        pass
+    def analyze(self):
+        if len(self.file_list) == 0:
+            MessageDialogEnd(error=True, log_file=self.log_name, title=self.prog_name, msg1="ERROR",
+                             msg2=self.msg_not_found)
+            self.logNB.error(self.msg_not_found)
+            sys.exit(1)
+        elif self.error:
+            MessageDialogEnd(error=True, log_file=self.log_name, title=self.prog_name, msg1="ERROR",
+                             msg2=self.msg_end)
+            self.logNB.error(self.msg_end)
+            sys.exit(1)
+        else:
+            MessageDialogEnd(error=False, log_file=self.log_name, title=self.prog_name, msg1="OK",
+                             msg2=self.msg_end)
 
     def run(self):
-        self.logNB.info("In  run")
-        self.parsingLine()
         self.getFileList()
-        self.checkFileList("No image has been found.")
-        self.convert()
+        if len(self.file_list) != 0:
+            self.convert()
+        self.analyze()
         # self.runJob()
-        self.analyzeJob()
-        # (file_list, count_warn) = listFromArgs(log, HEADER, args, auth_ext)
-
-    ## End dialog
-    # MessageDialogEnd(count_warn, count_error, logFile, "Convert images", "\nJob fini.")
