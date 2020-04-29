d475db74c9dcf67fe47cf552933c294e49eb8d4c
Gregory Brancq <gregory.brancq@free.fr>
2020-04-29 13:53:45 +0200

    Renamed file.
    Change-Id: Ie630ec4283eb545e3eebc6bf94bb5666184bb37c

---
+++ convertMp42Mp3.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Convert MP4 to MP3
+"""
+
+# Import
+import logging
+
+from common import createLog, parsingLine
+from nemoBase import NemoBase
+
+
+class ConvertMp42Mp3(NemoBase):
+    def __init__(self, root_log, args):
+        root_log_name = '.'.join([root_log, self.__class__.__name__])
+        self.logCB = logging.getLogger(root_log_name)
+        super().__init__(root_log, root_log_name, args)
+
+    def run(self):
+        command = 'pacpl --to mp3 -bitrate 320'
+        command_param = False
+        auth_ext = [".mp4", ".MP4"]
+        res_ext = ".mp3"
+        msg_not_found = "No video has been found."
+        self.setConfig(command, command_param, auth_ext, res_ext, msg_not_found)
+        self.runJob()
+
+
+
+def main():
+    # Create log class
+    root_log = 'convertMp42Mp3'
+    (parsedArgs, args) = parsingLine()
+    logger = createLog(root_log, parsedArgs)
+    logger.info("START")
+    ConvertMp42Mp3(root_log, args).run()
+    logger.info("STOP")
+
+
+if __name__ == '__main__':
+    main()
