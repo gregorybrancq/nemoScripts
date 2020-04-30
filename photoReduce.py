0666edc8875eedc40d98da69ad1c12948883a96b
Gregory Brancq <gregory.brancq@free.fr>
2020-04-30 16:29:21 +0200

    Updated photoReduce.py
    Change-Id: I2ca976b15903291319571ecc054a00dc74dfc234

--- photoReduce.py
+++ photoReduce.py
@@ -1,163 +1,59 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
-Reduce photo size
-'''
+"""
+Convert bmp to jpg
+"""
 
-
-
-## Import
+# Import
 import sys
-import os
-from datetime import datetime
-import subprocess
-from optparse import OptionParser
+import logging
 
-## common
-from python_common import *
-HEADER = "photoReduce"
+from common import createLog, parsingLine
+from nemoBase import NemoBase
 
-## directory
-logDir = getLogDir()
-
-###############################################
+sys.path.append('/home/greg/Greg/work/env/pythonCommon')
+from message import MessageDialog
 
 
+class PhotoReduce(NemoBase):
+    def __init__(self, root_log, args):
+        root_log_name = '.'.join([root_log, self.__class__.__name__])
+        self.logCB = logging.getLogger(root_log_name)
+        super().__init__(root_log, root_log_name, args)
 
-###############################################
-###############################################
-##              Line Parsing                 ##
-###############################################
-###############################################
-
-parsedArgs = {}
-parser = OptionParser()
-
-
-parser.add_option(
-    "-d",
-    "--debug",
-    action  = "store_true",
-    dest    = "debug",
-    default = False,
-    help    = "Display all debug information"
-    )
-
-(parsedArgs , args) = parser.parse_args()
-
-###############################################
-
-
-
-###############################################
-## Global variables
-###############################################
-
-t = str(datetime.today().isoformat("_"))
-logFile = os.path.join(logDir, HEADER + "_" + t + ".log")
-errC = 0
-resizePercent = 100
-winTitle = "Reduce photo size"
-
-###############################################
-
-
-
-
-
-###############################################
-###############################################
-##                FUNCTIONS                  ##
-###############################################
-###############################################
-
-def reduceFile(fileList) :
-    global log
-    global errC
-    log.info(HEADER, "In  reduceFile")
-
-    oldDir = os.getcwd()
-
-    for (fileD, fileN, fileE) in fileList :
-        log.info(HEADER, "In  reduceFile directory " + str(fileD) + "  reduceFile " + fileN + fileE)
-
-        if (fileD != "") :
-            os.chdir(fileD)
-
-        cmd='mogrify -resize ' + str(resizePercent) + '% ' + fileN + fileE
-        log.info(HEADER, "In  reduceFile cmd=" + str(cmd))
-        procPopen = subprocess.Popen(cmd, shell=True, stderr=subprocess.STDOUT)
-        procPopen.wait()
-        if (procPopen.returncode != 0) :
-            errC += 1
-            log.error(HEADER, "In  reduceFile file: issue with " + str(os.path.join(fileD, fileN + fileE)))
-
-        if (fileD != "") :
-            os.chdir(oldDir)
-
-    log.info(HEADER, "Out reduceFile")
-
-###############################################
-
-
-
-
-
-
-###############################################
-###############################################
-###############################################
-##                 MAIN                      ##
-###############################################
-###############################################
-###############################################
-
-
-def main() :
-    global log
-    global resizePercent
-    warnC = 0
-    log.info(HEADER, "In  main")
-
-    fileList = list()
-
-    log.info(HEADER, "In  main parsedArgs=" + str(parsedArgs))
-    log.info(HEADER, "In  main args=" + str(args))
-
-    ## Create list of files
-    extAuth=[".jpg", ".JPG", ".jpeg", ".JPEG", ".tif", ".TIF", ".gif", ".GIF", ".bmp", ".BMP"]
-    (fileList, warnC) = listFromArgs(log, HEADER, args, extAuth)
-
-    ## Verify if there is at least one file to convert
-    if (len(fileList) == 0) :
-        log.exit("Reduce photo size", "No image has been found\n")
-    else :
-        resizePercentStr = MessageDialog(type_='entry', title=winTitle, message="Quelle est le pourcentage de réduction ?").run()
+    def askPercent(self):
+        win_title = "Reduce photo size"
+        resize_percent_str = MessageDialog(dialog_type='entry', title=win_title, message1="Quelle est le pourcentage de réduction ?").run()
         try :
-            resizePercent=int(resizePercentStr)
+            resize_percent=int(resize_percent_str)
         except ValueError :
-            MessageDialog(type_='error', title=winTitle, message="\nle pourcentage doit être un chiffre entre 0 et 100.\n").run()
-            log.exit(HEADER, "In  main resizePercent is not an integer")
+            MessageDialog(type_='error', title=win_title, message="\nle pourcentage doit être un chiffre entre 0 et 100.\n").run()
+            sys.exit(1)
+        self.logCB.debug("resize_percent=%s"%str(resize_percent))
+        return resize_percent
 
-    ## Convert them
-    log.dbg("fileList="+str(fileList))
-    reduceFile(fileList)
+    def run(self):
+        resize_percent = self.askPercent()
+        command='mogrify -resize ' + str(resize_percent) + '%'
+        command_param = False
+        delete_file = False
+        auth_ext = [".jpg", ".JPG", ".jpeg", ".JPEG", ".tif", ".TIF", ".gif", ".GIF", ".bmp", ".BMP"]
+        res_ext = ""
+        msg_not_found = "No image has been found."
+        self.setConfig(command, command_param, delete_file, auth_ext, res_ext, msg_not_found)
+        self.runOneCommand()
 
-    ## End dialog
-    MessageDialogEnd(warnC, errC, logFile, winTitle, "\nJob fini.")
-    
-    log.info(HEADER, "Out main")
 
-###############################################
-
-
+def main():
+    # Create log class
+    root_log = 'photoReduce'
+    (parsedArgs, args) = parsingLine()
+    logger = createLog(root_log, parsedArgs)
+    logger.info("START")
+    PhotoReduce(root_log, args).run()
+    logger.info("STOP")
 
 
 if __name__ == '__main__':
- 
-    ## Create log class
-    log = LOGC(logFile, HEADER, parsedArgs.debug)
     main()
-
-
