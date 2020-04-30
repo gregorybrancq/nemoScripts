ba215e5a0fe9dbfd0c053e9ddc0cff72e88367d1
Gregory Brancq <gregory.brancq@free.fr>
2020-04-30 18:04:38 +0200

    Updated renameFiles.py
    Change-Id: I6e96f9ab5c7ab72f9e8e3787d4e69c313eec60ab

--- duplicateFile.py
+++ duplicateFile.py
@@ -26,7 +26,7 @@ class DuplicateFile(NemoBase):
         res_ext = ""
         msg_not_found = "No file has been found."
         self.setConfig(command, command_param, delete_file, auth_ext, res_ext, msg_not_found)
-        self.runProgram()
+        self.runProgram(on_dir=True)
 
 
 def main():
