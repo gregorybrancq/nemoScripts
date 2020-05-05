28af609a946bfecbb263b2a021c97675764974c2
Gregory Brancq <gregory.brancq@free.fr>
2020-05-05 16:20:40 +0200

    Added splitFlacWithCue.py
    Change-Id: If73c7197bdd67a42b526403e5954a346d9c41696

--- splitFlacWithCue.py
+++ splitFlacWithCue.py
@@ -1,188 +1,105 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
-Split flac file into several thanks to cue file
-'''
+"""
+Split flac file into several ones thanks to cue file
+"""
 
-
-
-## Import
-import sys
+# Import
+import logging
 import os
-from datetime import datetime
 import subprocess
-from optparse import OptionParser
+import sys
 
-## common
-from python_common import *
-HEADER = "SpliFlac"
-
-## directory
-logDir   = getLogDir()
-
-###############################################
+from common import createLog, parsingLine
+from message import MessageDialogEnd
+from nemoBase import NemoBase
 
 
+class SplitFlacWithCue(NemoBase):
+    def __init__(self, root_log, args):
+        root_log_name = '.'.join([root_log, self.__class__.__name__])
+        self.logSFC = logging.getLogger(root_log_name)
+        super().__init__(root_log, root_log_name, args)
 
-###############################################
-###############################################
-##              Line Parsing                 ##
-###############################################
-###############################################
+    def warningInputFiles(self):
+        MessageDialogEnd(error=True, log_file=self.log_name, title=self.prog_name,
+                         msg1="ERROR", msg2=self.msg_not_found)
+        self.logSFC.error(self.msg_not_found)
+        sys.exit(1)
 
-parsedArgs = {}
-parser = OptionParser()
+    def compute(self):
+        file_flac = ""
+        dir_flac = ""
+        file_cue = ""
+        dir_cue = ""
+        for (dir_name, file_name, file_ext) in self.file_list_in:
+            self.logSFC.debug("dir = %s, file = %s, ext = %s" % (dir_name, file_name, file_ext))
+            if dir_name != "":
+                os.chdir(dir_name)
 
+            if file_ext == ".flac":
+                file_flac = file_name + file_ext
+                dir_flac = dir_name
+            elif file_ext == ".cue":
+                file_cue = file_name + file_ext
+                dir_cue = dir_name
 
-parser.add_option(
-    "-d",
-    "--debug",
-    action  = "store_true",
-    dest    = "debug",
-    default = False,
-    help    = "Display all debug information"
-    )
+        if (not file_flac or not file_cue) and dir_cue != dir_flac:
+            self.warningInputFiles()
 
-(parsedArgs , args) = parser.parse_args()
+        # Construct the command
+        if dir_flac:
+            os.chdir(dir_flac)
+        self.command = 'shntool split'
+        cmd = self.command.split(" ")
+        cmd.append('-t')
+        cmd.append('%p - %a - %n - %t')
+        cmd.append("-f")
+        cmd.append(file_cue)
+        cmd.append("-o")
+        cmd.append("flac")
+        cmd.append(file_flac)
 
-###############################################
+        self.logNB.info("Run command %s" % str(cmd))
 
+        # Execute the command
+        process = subprocess.Popen(cmd, stderr=subprocess.STDOUT)
+        process.wait()
+        if process.returncode != 0:
+            self.error = True
+            self.msg_end += "In %s, cmd failed : \n  %s\n" % (os.getcwd(), str(cmd))
+        else:
+            self.msg_end += "Executed :\n  %s\n  %s\n" % (file_flac, file_cue)
 
+    def run(self):
+        command = "shntool"
+        command_options = ""
+        command_set_output = False
+        delete_file = False
+        auth_ext = [".flac", ".FLAC", ".cue", ".CUE"]
+        res_ext = ""
+        msg_not_found = "Need exactly 2 audio files in same directory : .flac and .cue."
+        self.setConfig(command, command_options, command_set_output, delete_file,
+                       auth_ext, res_ext, msg_not_found)
 
-###############################################
-## Global variables
-###############################################
-
-t = str(datetime.today().isoformat("_"))
-logFile = os.path.join(logDir, HEADER + "_" + t + ".log")
-errC = 0
-fileFlac = str()
-
-###############################################
-
-
-
-
-
-###############################################
-###############################################
-##                FUNCTIONS                  ##
-###############################################
-###############################################
-
-def splitFlac(fileList) :
-    global log
-    global errC
-    global fileFlac
-    log.info(HEADER, "In  splitFlac")
-
-    oldDir = os.getcwd()
-
-    (file1D, file1N, file1E) = fileList[0]
-    (file2D, file2N, file2E) = fileList[1]
-    log.info(HEADER, "In  splitFlac file1=" + os.path.join(file1D, file1N + file1E))
-    log.info(HEADER, "In  splitFlac file2=" + os.path.join(file2D, file2N + file2E))
-
-    if ( (file1E == ".cue") or (file1E == ".CUE") ) and ((file2E == ".flac") or (file2E == ".FLAC") ) :
-        fileCueD = file1D
-        fileCueN = file1N
-        fileCueE = file1E
-        fileFlacD = file2D
-        fileFlacN = file2N
-        fileFlacE = file2E
-    elif ( (file2E == ".cue") or (file2E == ".CUE") ) and ((file1E == ".flac") or (file1E == ".FLAC") ) :
-        fileCueD = file2D
-        fileCueN = file2N
-        fileCueE = file2E
-        fileFlacD = file1D
-        fileFlacN = file1N
-        fileFlacE = file1E
-    else :
-        MessageDialog(type_='error', title="Split Flac with Cue", message="\nThe 2 files must be flac AND cue.\n").run()
-
-    log.info(HEADER, "In  splitFlac fileCue=" + os.path.join(fileCueD, fileCueN + fileCueE))
-    log.info(HEADER, "In  splitFlac fileFlac=" + os.path.join(fileFlacD, fileFlacN + fileFlacE))
-    fileFlac = os.path.join(fileFlacD, fileFlacN + fileFlacE)
-
-    if (fileFlacD != fileCueD) :
-        MessageDialog(type_='error', title="Split Flac with Cue", message="\nThe 2 files flac and cue must be in the same directory.\n").run()
-
-    if (fileFlacD != "") :
-        os.chdir(fileFlacD)
-
-    cmd='shntool split -t "%p - %a - %n - %t" -f "' + fileCueN + fileCueE + '" -o flac "' + fileFlacN + fileFlacE + '"'
-    log.info(HEADER, "In  splitFlac cmd=" + str(cmd))
-    procPopen = subprocess.Popen(cmd, shell=True, stderr=subprocess.STDOUT)
-    log.info(HEADER, "In  splitFlac before wait")
-    procPopen.wait()
-    log.info(HEADER, "In  splitFlac after wait returnCode = " + str(procPopen.returncode))
-    if (procPopen.returncode != 0) :
-        errC += 1
-        log.error(HEADER, "In  splitFlac file: issue with " + str(os.path.join(fileFlacD, fileFlacN + fileFlacE)))
-
-    log.info(HEADER, "In  splitFlac after check")
-    if (fileFlacD != "") :
-        os.chdir(oldDir)
-
-    log.info(HEADER, "Out splitFlac")
-
-###############################################
+        self.getFileList()
+        if len(self.file_list_in) != 2:
+            self.warningInputFiles()
+        else:
+            self.compute()
+        self.analyze()
 
 
-
-
-
-
-###############################################
-###############################################
-###############################################
-##                 MAIN                      ##
-###############################################
-###############################################
-###############################################
-
-
-def main() :
-    global log
-    warnC = 0
-    log.info(HEADER, "In  main")
-
-    fileList = list()
-
-    log.info(HEADER, "In  main parsedArgs=" + str(parsedArgs))
-    log.info(HEADER, "In  main args=" + str(args))
-
-    ## Create list of files
-    extAuth=[".flac", ".FLAC", ".cue", ".CUE"]
-    (fileList, warnC) = listFromArgs(log, HEADER, args, extAuth)
-
-    ## Verify if there is at least one file to convert
-    if (len(fileList) == 0) :
-        MessageDialog(type_='error', title="Split Flac with Cue", message="\nNo flac and cue file have been found\n").run()
-    elif (len(fileList) != 2) :
-        MessageDialog(type_='error', title="Split Flac with Cue", message="\nOnly 2 files are allowed = flac and cue\n").run()
-
-    log.info(HEADER, "In  main filelist = " + str(len(fileList)))
-
-    ## Split it
-    splitFlac(fileList)
-
-    ## End dialog
-    MessageDialogEnd(warnC, errC, logFile, "Convert music files", "\nJob fini : " + str(fileFlac) + " splitted.")
-    
-    log.info(HEADER, "Out main")
-
-###############################################
-
-
+def main():
+    # Create log class
+    root_log = 'splitFlac'
+    (parsedArgs, args) = parsingLine()
+    logger = createLog(root_log, parsedArgs)
+    logger.info("START")
+    SplitFlacWithCue(root_log, args).run()
+    logger.info("STOP")
 
 
 if __name__ == '__main__':
- 
-    ## Create log class
-    log = LOGC(logFile, HEADER, parsedArgs.debug)
-
     main()
-
-
